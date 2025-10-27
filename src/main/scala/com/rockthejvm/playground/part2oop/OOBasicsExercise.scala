15286aeec89014250216f95a664698b04187d9d4
Dorothea87 <147050615+Dorothea87@users.noreply.github.com>
2025-10-27 16:55:29 +0000

    practice basic OOP

--- src/main/scala/com/rockthejvm/playground/part2oop/OOBasicsExercise.scala
+++ src/main/scala/com/rockthejvm/playground/part2oop/OOBasicsExercise.scala
@@ -1,5 +1,7 @@
 package com.rockthejvm.playground.part2oop
 
+import java.io.FileReader
+
 object OOBasicsExercise {
 
   //EXERCISE
@@ -11,8 +13,33 @@ object OOBasicsExercise {
   //copy new year of release returns a new car instance with same data except the new year of release
   // - Driver: frist name, last name, YOB, method full name -> concatenate
 
+  class Car(make: String, model: String, yearOfRelease: Int, owner: Driver) {
+    def ownerAge(): Int = yearOfRelease - owner.YOB
+    def isOwnedBy(driver: Driver): Boolean = driver == owner
+
+    def copy(newYearOfRelease: Int): Car =
+      new Car(make, model, newYearOfRelease, owner)
+
+  }
+
+  class Driver(firstName: String, lastName: String, val YOB: Int) {
+    def fullName: String = {
+      s"The owner of this car is: $firstName $lastName."
+    }
+  }
+
+  val peter = new Driver("Peter", "Parker", 1975)
 
   def main(args: Array[String]): Unit = {
-
+  println(peter.fullName)
+    val driver = new Driver("Michael", "Schuhmacher", 1969)
+    val driverImpostor = new Driver("Michael", "Schuhmacher", 2020)
+    val car = new Car("Ferrari", "F1", 2004, driver)
+    println(driver.fullName)
+    println(car.ownerAge())
+    println(car.isOwnedBy(driver))
+    println(car.isOwnedBy(driverImpostor))
+    println(car.copy(2005).ownerAge())
+    println(s"testing equality: ${driver == driverImpostor}")
   }
 }
